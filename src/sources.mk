ba98098da31bafeaec5ebd8624b04385f6901478
hatemalamir <hatem.alamir.90@gmail.com>
2024-11-28 19:02:49 -0500

    Fix syntac error of depency file generation target. Fix INCLUDES. Change -mfloat-abi=hard to -mfloat-abi=soft to fix warnings

--- src/sources.mk
+++ src/sources.mk
@@ -13,16 +13,17 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-ifeq ($(PLATFORM),HOST)
-	SOURCES = main.c \
-			  memory.c
-else
+# Add your include paths to this variable
+ifeq ($(PLATFORM),MSP432)
 	SOURCES = main.c \
 			  memory.c \
 			  interrupts_msp432p401r_gcc.c \
 			  startup_msp432p401r_gcc.c \
 			  system_msp432p401r.c
+	INCLUDES = -I../include/common -I../include/msp432 -I../include/CMSIS
+else
+	SOURCES = main.c \
+			  memory.c
+	INCLUDES = -I../include/common
 endif
 
-# Add your include paths to this variable
-INCLUDES = ../include
